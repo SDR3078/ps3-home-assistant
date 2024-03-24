12602bca3527098d8ce9cba35d8a4ed5e67c75d5
SDR3078 <sander.dereu@outlook.com>
2024-03-24 15:15:54 +0100

    Climate entity

--- custom_components/ps3/API/PS3MAPI.py
+++ custom_components/ps3/API/PS3MAPI.py
@@ -12,6 +12,9 @@ class NotificationError(Exception):
 class FanModeError(Exception):
     pass
 
+class RequestError(Exception):
+    pass
+
 class PS3MAPIWrapper:
     def __init__(self, ip: str):
         self.ip = ip
@@ -20,6 +23,7 @@ class PS3MAPIWrapper:
         self._rsx_temp = None
         self._fan_speed = None
         self._fan_mode = None
+        self._target_temp = None
         self._fan_modes_mapping = {
             'SYSCON': 'SYSCON',
             'Manual': 'Manual',
@@ -37,10 +41,10 @@ class PS3MAPIWrapper:
                         self._state = "On"
                         soup = BeautifulSoup(await response.text(), 'html.parser')
                         temperature_text = soup.find('a', class_='s', href = '/cpursx.ps3?up').text
-                        self._cpu_temp = temperature_text.split(': ')[1].split('°C')[0]
-                        self._rsx_temp = temperature_text.split(': ')[-1].split('°C')[0]
+                        self._cpu_temp = float(temperature_text.split(': ')[1].split('°C')[0])
+                        self._rsx_temp = float(temperature_text.split(': ')[-1].split('°C')[0])
                         fan_speed_text = soup.find('a', class_='s', href = '/cpursx.ps3?mode').text
-                        self._fan_speed = fan_speed_text.split(': ')[1].split('%')[0]
+                        self._fan_speed = int(fan_speed_text.split(': ')[1].split('%')[0])
 
                         for substring, fan_mode in self._fan_modes_mapping.items():
                             if substring in temperature_text:
@@ -49,12 +53,18 @@ class PS3MAPIWrapper:
                         else:
                             raise SensorError("Fan mode sensor does not work")
                         
+                        if self._fan_mode == 'Dynamic':
+                            self._target_temp = float(temperature_text.split(': ')[2].split('°C')[0])
+                        else:
+                            self._target_temp = None
+
                     else:
                         self._state = None
                         self._cpu_temp = None
                         self._rsx_temp = None
                         self._fan_speed = None
                         self._fan_mode = None
+                        self._target_temp = None
                         raise SensorError(f"Unexpected response code: {response.status}")
         except asyncio.TimeoutError:
             self._state = "Off"
@@ -62,6 +72,7 @@ class PS3MAPIWrapper:
             self._rsx_temp = None
             self._fan_speed = None
             self._fan_mode = None
+            self._target_temp = None
         except SensorError:
             raise
 
@@ -88,7 +99,7 @@ class PS3MAPIWrapper:
 
         try:
             fan_mode_substring = url_substrings[fan_mode]
-            endpoint_fan_mode = f"http://{self.ip}/cpursx.ps3?{fan_mode_substring}"
+            endpoint_fan_mode = f"http://{self.ip}/cpursx.ps3?{fan_mode_substring};/beep.ps3?1"
 
             async with aiohttp.ClientSession() as session:
                 async with session.get(endpoint_fan_mode, timeout = 5) as response:
@@ -102,6 +113,25 @@ class PS3MAPIWrapper:
             raise
         except KeyError:
             raise FanModeError("Fan mode does not exist")
+        except Exception:
+            raise FanModeError("Invalid host")
+        
+    async def _set_target_temp(self, target_temp: int):
+        endpoint_target_temp = f"http://{self.ip}/cpursx.ps3?max={target_temp};/beep.ps3?1"
+        
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(endpoint_target_temp, timeout = 5) as response:
+                    if response.status == 200:
+                        pass
+                    else:
+                        raise RequestError(f"Unexpected response code: {response.status}")
+        except asyncio.TimeoutError:
+            raise RequestError("Request is not available")
+        except RequestError:
+            raise
+        except Exception:
+            raise RequestError("Invalid host")
 
 
     async def update(self):
@@ -120,6 +150,12 @@ class PS3MAPIWrapper:
         try:
             await self._set_fan_mode(fan_mode)
         except FanModeError:
+            raise
+
+    async def set_target_temp(self, target_temp: int):
+        try:
+            await self._set_target_temp(target_temp)
+        except RequestError:
             raise
 
     @property
@@ -141,3 +177,11 @@ class PS3MAPIWrapper:
     @property
     def fan_mode(self):
         return self._fan_mode
+    
+    @property
+    def target_temp(self):
+        return self._target_temp
+    
+    @property
+    def fan_modes(self):
+        return list(self._fan_modes_mapping.values())
--- custom_components/ps3/const.py
+++ custom_components/ps3/const.py
@@ -1,5 +1,7 @@
 DOMAIN = "ps3"
-PLATFORMS = ["binary_sensor", "sensor"]
+PLATFORMS = ["binary_sensor", "sensor", "climate"]
 DATA_HASS_CONFIG = "ps3_hass_config"
 CONF_ENTRY_ID = "entry_id"
 ENTRIES = "entries"
+MAX_TEMP = 85
+MIN_TEMP = 40
